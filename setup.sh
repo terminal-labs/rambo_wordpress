b92086cfc59ac002dc975f940d92df5e005fac3d
Michael Verhulst <michael@terminallabs.com>
2017-12-07 13:36:21 -0600

    conforming to new rambo standard

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cp auth/env_scripts/digitalocean.env.sh.dist digitalocean.env.sh
-cp auth/env_scripts/aws.env.sh.dist aws.env.sh
-
-vagrant plugin install vagrant-aws
-vagrant plugin install vagrant-digitalocean
-vagrant plugin install vagrant-lxc
-vagrant plugin install vagrant-triggers
-vagrant plugin install vagrant-scp
